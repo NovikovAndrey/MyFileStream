d2fdb7f50605a38000d01eb024d2bbc48557ab58
Андрей Новиков <novikov.andrey.v@hotmail.com>
2020-02-13 16:40:38 +0300

    Write-Read

--- MyFileStream/Program.cs
+++ MyFileStream/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace MyFileStream
 {
@@ -6,7 +7,27 @@ namespace MyFileStream
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"D:\";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Enter string to wtite");
+            string text = Console.ReadLine();
+            using (FileStream fStream = new FileStream($"{path}note.txt", FileMode.OpenOrCreate))
+            {
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                fStream.Write(array, 0, array.Length);
+                Console.WriteLine("End write");
+            }
+            using (FileStream rStream = File.OpenRead($"{path}note.txt"))
+            {
+                byte[] array = new byte[rStream.Length];
+                rStream.Read(array, 0, array.Length);
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine(textFromFile);
+            }
         }
     }
 }
